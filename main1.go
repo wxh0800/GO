711338dd5c018aaf8dfe74f921f1a1df4f31b493
William Wang <wxh0800@gmail.com>
2020-12-24 16:42:04 -0500

    2nd update

--- main1.go
+++ main1.go
@@ -1,6 +1,6 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
 // example 1
 // func main()  {
@@ -32,13 +32,85 @@ import "fmt"
 // 	fmt.Println("my name is", name, "age is", age2)
 // }
 
-func rectProps(length, width float64) (float64, float64) {
-	var area = length * width
-	var perimeter = (length + width) * 2
-	return area, perimeter
-}
+// func rectProps(length, width float64) (float64, float64) {
+// 	var area = length * width
+// 	var perimeter = (length + width) * 2
+// 	return area, perimeter
+// }
+
+// func main() {
+// 	area, perimeter := rectProps(10.8, 5.6)
+// 	fmt.Printf("Area %f Perimeter %f", area, perimeter)
+// }
+package main
+
+import (
+	"fmt"
+)
+
+// func main() {
+// 	pls := [][]string{
+// 		{"C", "C++"},
+// 		{"JavaScript"},
+// 		{"Go", "Rust"},
+// 	}
+// 	for _, v1 := range pls {
+// 		for _, v2 := range v1 {
+// 			fmt.Printf("%s ", v2)
+// 		}
+// 		fmt.Printf("\n")
+// 	}
+// }
+
+// func find(num int, nums ...int) {
+// 	fmt.Printf("type of nums is %T\n", nums)
+// 	found := false
+// 	for i, v := range nums {
+// 		if v == num {
+// 			fmt.Println(num, "found at index", i, "in", nums)
+// 			found = true
+// 		}
+// 	}
+// 	if !found {
+// 		fmt.Println(num, "not found in ", nums)
+// 	}
+// 	fmt.Printf("\n")
+// }
+// func main() {
+// 	find(89, 89, 90, 95)
+// 	find(45, 56, 67, 45, 90, 109)
+// 	find(78, 38, 56, 98)
+// 	find(87)
+// }
+
+// func find(num int, nums ...int) {
+// 	fmt.Printf("type of nums is %T\n", nums)
+// 	found := false
+// 	for i, v := range nums {
+// 		if v == num {
+// 			fmt.Println(num, "found at index", i, "in", nums)
+// 			found = true
+// 		}
+// 	}
+// 	if !found {
+// 		fmt.Println(num, "not found in ", nums)
+// 	}
+// 	fmt.Printf("\n")
+// }
+// func main() {
+// 	nums := []int{89, 90, 95}
+// 	find(89, nums...)
+// }
 
 func main() {
-	area, perimeter := rectProps(10.8, 5.6)
-	fmt.Printf("Area %f Perimeter %f", area, perimeter)
+	personSalary := map[string]int{
+		"steve": 12000,
+		"jamie": 15000,
+	}
+	personSalary["mike"] = 9000
+	fmt.Println("Original person salary", personSalary)
+	newPersonSalary := personSalary
+	newPersonSalary["mike"] = 18000
+	fmt.Println("Person salary changed", personSalary)
+
 }
